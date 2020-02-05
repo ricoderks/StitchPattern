9072eebff9c76e66395c60e75b6e81f6beb3d71a
Rico Derks <r.j.e.derks@lumc.nl>
2020-02-05 21:05:55 +0100

    some small fixes

--- cross_stitch.R
+++ cross_stitch.R
@@ -23,7 +23,6 @@ final_img <- image_quantize(my_img_small,
 #             path = "./butterfly_75.jpg",
 #             format = "jpg")
 
-
 ## convert to cimg class for further processing
 c_img <- magick2cimg(final_img)
 
@@ -58,19 +57,36 @@ dmc_m <- as.matrix(dmc_codes[, c("Red", "Green", "Blue", "RGB code")])
 plan(multiprocess)
 uniq_code <- df_img %>% 
   distinct(rgb_code, .keep_all = TRUE) %>% 
-  mutate(dmc_rgb_code = future_pmap(.l = list(c1, c2, c3),
-                                    .f = ~ calc_dist2(dmc = dmc_m,
-                                                      red = ..1,
-                                                      green = ..2,
-                                                      blue = ..3)))
+  mutate(dmc_rgb_code = future_pmap_chr(.l = list(c1, c2, c3),
+                                        .f = ~ calc_dist2(dmc = dmc_m,
+                                                          red = ..1,
+                                                          green = ..2,
+                                                          blue = ..3)))
 
 df_img <- df_img %>% 
   left_join(y = uniq_code[, c("rgb_code", "dmc_rgb_code")],
             by = c("rgb_code" = "rgb_code"))
 
 ## create symbols for rgb color codes
-my_symbols <- data.frame(rgb_code = unique(df_img$rgb_code),
-                         symbols = unlist(strsplit(rawToChar(as.raw(33:126)), split = ""))[1:max_colors])
+my_symbols <- c(letters, 
+                LETTERS, 
+                unlist(strsplit("0123456789!@#$%^&*()-=_+\\|/<>?~", split = "")),
+                paste0(letters, letters),
+                paste0(LETTERS, LETTERS),
+                paste0(letters, unlist(strsplit("0123456789!@#$%^&*()-=_+\\|/<>?~", split = ""))),
+                paste0(LETTERS, unlist(strsplit("0123456789!@#$%^&*()-=_+\\|/<>?~", split = ""))))
+df_symbols <- data.frame(dmc_rgb_code = unique(df_img$dmc_rgb_code))
+df_symbols <- df_symbols %>% 
+  # add symbols
+  mutate(symbols = my_symbols[1:n()]) %>% 
+  # add dmc info
+  left_join(y = dmc_codes,
+            by = c("dmc_rgb_code" = "RGB code"))
+
+# add symbols to image
+df_img <- df_img %>% 
+  left_join(y = df_symbols,
+            by = c("dmc_rgb_code" = "dmc_rgb_code"))
 
 ## create table for legend
 # at the moment this is without DMC thread names
@@ -80,21 +96,23 @@ pdf(file = "my_table.pdf",
     height = 11.7,
     paper = "a4")
 # create the tables
-limits <- floor(seq(1, nrow(my_symbols), length.out = 4))
-g1 <- tableGrob(my_symbols[limits[1]:limits[2], ])
-g2 <- tableGrob(my_symbols[(limits[2] + 1):limits[3], ])
-g3 <- tableGrob(my_symbols[(limits[3] + 1):limits[4], ])
+limits <- floor(seq(1, nrow(df_symbols), length.out = 3))
+g1 <- tableGrob(df_symbols[limits[1]:limits[2], c("symbols", "Floss#", "Description")])
+g2 <- tableGrob(df_symbols[(limits[2] + 1):limits[3], c("symbols", "Floss#", "Description")])
+# g3 <- tableGrob(my_symbols[(limits[3] + 1):limits[4], c("symbols", "Floss#", "Description")])
 # align the tables
+# 1st page
 grid.arrange(
-  gtable_combine(g1, g2, g3, along = 1),
+  gtable_combine(g1, g2, along = 1),
   nrow = 1
 )
+# # 2nd page
+# grid.arrange(
+#   gtable_combine(g3, along = 1),
+#   nrow = 1
+# )
 #save
 dev.off()
-
-df_img <- df_img %>% 
-  left_join(y = my_symbols,
-            by = "rgb_code")
 
 ## plot per page
 # initialize plot list
@@ -114,10 +132,6 @@ for (xx in 1:step_w) {
   p_list[[page_count]] <- df_img %>% 
     ggplot(aes(x = x,
                y = y)) +
-    # # use this one to show the picture
-    # geom_tile(aes(fill = rgb_code),
-    #           colour = "grey50") +
-    # scale_fill_identity() +
     # put the symbols in
     geom_text(aes(label = symbols),
               size = 2) +
@@ -158,30 +172,30 @@ system.time(
 )
 
 ##  show complete pattern
-df_img %>% 
-  ggplot(aes(x = x,
-             y = y)) +
-  # # use this one to show the picture
-  geom_tile(aes(fill = dmc_rgb_code)
-            # colour = "grey50"
-            ) +
-  scale_fill_identity() +
-  # put the symbols in
-  # geom_text(aes(label = symbols),
-  #           size = 1) +
-  # modify y-axis
-  scale_y_continuous(trans = "reverse",
-                     limits = c(step_h * num_step_h, 0),
-                     breaks = c(1, seq(5, step_h * num_step_h, 5)),
-                     minor_breaks = c(1.5, seq(1, step_h * num_step_h, 1) + 0.5),
-                     expand = expand_scale(add = -0.5)) +
-  # modify x-axis
-  scale_x_continuous(limits = c(1, step_w * num_step_w),
-                     breaks = c(1, seq(0, step_w * num_step_w, 5)),
-                     minor_breaks = c(1.5, seq(1, step_w * num_step_w , 1) + 0.5),
-                     expand = expand_scale(add = -0.5)) +
-  # play around with the theme
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_line(colour = "grey"),
-        panel.background = element_blank(),
-        axis.line = element_line(colour = "black"))
+# df_img %>% 
+#   ggplot(aes(x = x,
+#              y = y)) +
+#   # # use this one to show the picture
+#   geom_tile(aes(fill = dmc_rgb_code)
+#             # colour = "grey50"
+#             ) +
+#   scale_fill_identity() +
+#   # put the symbols in
+#   # geom_text(aes(label = symbols),
+#   #           size = 1) +
+#   # modify y-axis
+#   scale_y_continuous(trans = "reverse",
+#                      limits = c(step_h * num_step_h, 0),
+#                      breaks = c(1, seq(5, step_h * num_step_h, 5)),
+#                      minor_breaks = c(1.5, seq(1, step_h * num_step_h, 1) + 0.5),
+#                      expand = expand_scale(add = -0.5)) +
+#   # modify x-axis
+#   scale_x_continuous(limits = c(1, step_w * num_step_w),
+#                      breaks = c(1, seq(0, step_w * num_step_w, 5)),
+#                      minor_breaks = c(1.5, seq(1, step_w * num_step_w , 1) + 0.5),
+#                      expand = expand_scale(add = -0.5)) +
+#   # play around with the theme
+#   theme(panel.grid.major = element_blank(),
+#         panel.grid.minor = element_line(colour = "grey"),
+#         panel.background = element_blank(),
+#         axis.line = element_line(colour = "black"))
--- functions.R
+++ functions.R
@@ -7,6 +7,6 @@
 calc_dist2 <- function(dmc, red, green, blue) {
   dist_m <- pdist::pdist(dmc[, c("Red", "Green", "Blue")], c(red, green, blue))@dist
   min_idx <- which.min(dist_m[1:length(dist_m)])
-  rgb_code <- dmc[min_idx, "RGB code"]
+  rgb_code <- as.vector(dmc[min_idx, "RGB code"])
   return(rgb_code)
 }
